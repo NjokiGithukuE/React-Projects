1c3024c5dada2ee4cfe2ef79c13b871d89b96f03
NjokiGithukuE <njokigithuku20@gmail.com>
2024-06-21 22:37:00 +0300

    added firebase

---
+++ kanban-app-tutorial/utils/data.js
@@ -0,0 +1,26 @@
+export const id = () => Math.random().toString(36).substring(2, 10);
+
+export const data = {
+    "boards": [
+        {
+            id: id(), 
+            name: "Roadmap", 
+            columns: [
+                {
+                    id: id(), 
+                    name: "Now", 
+                    tasks: {
+                        id: id(), 
+                        title: "Launch version one", 
+                        status: "Now"
+                    }, 
+                    
+                        id: id(), 
+                        name: "Review early feedback and plan next steps for roadmap", 
+                        status: "Now"
+                    
+                }
+            ]
+        }
+    ]
+}
