907a90916c3dc2be29258c0dd93fd47cc6430509
NjokiGithukuE <njokigithuku20@gmail.com>
2024-06-22 23:52:11 +0300

    minor change

--- kanban-app-tutorial/utils/data.js
+++ kanban-app-tutorial/utils/data.js
@@ -1,26 +1,35 @@
 export const id = () => Math.random().toString(36).substring(2, 10);
 
 export const data = {
-    "boards": [
+  boards: [
+    {
+      id: id(),
+      name: "Roadmap",
+      columns: [
         {
-            id: id(), 
-            name: "Roadmap", 
-            columns: [
-                {
-                    id: id(), 
-                    name: "Now", 
-                    tasks: {
-                        id: id(), 
-                        title: "Launch version one", 
-                        status: "Now"
-                    }, 
-                    
-                        id: id(), 
-                        name: "Review early feedback and plan next steps for roadmap", 
-                        status: "Now"
-                    
-                }
-            ]
-        }
-    ]
-}
+          id: id(),
+          name: "Now",
+          tasks: {
+            id: id(),
+            title: "Launch version one",
+            status: "Now",
+          },
+
+          id: id(),
+          name: "Review early feedback and plan next steps for roadmap",
+          status: "Now",
+        },
+      ],
+    },
+    {
+      id: id(),
+      name: "Next",
+      tasks: [],
+    },
+    {
+      id: id(),
+      name: "Later",
+      tasks: [],
+    },
+  ],
+};
